a358aa175f276ee4b7c20dff41f82919195092ef
Roman <romski1988@gmail.com>
2021-01-28 16:07:36 +0000

    navigatio.php edited

--- navigation.php
+++ navigation.php
@@ -2,14 +2,17 @@
 $mydir = '.';
 $pages = scandir($mydir);
 
-echo '<div class ="menu">';
+echo '<div class="menu">';
 
 foreach($pages as $page){
-    if(strpos($page,'.php')){
+    if(strpos($page, '.php')){
         $link = $mydir.'/'.$page;
-        $safeName = str_replace('.php','',$page);
-        echo '<a href="'.$link'">'.$safeName.'</a>&nbsp;&nbsp;';
+        $safeName = str_replace('.php', '', $page);
+        echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
     }
+
 }
-echo '</div'>;
+
+echo '</div>';
+
 ?>
