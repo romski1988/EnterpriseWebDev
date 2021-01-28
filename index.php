611122e220abfef8567bbadacb83090752b237dd
Roman <romski1988@gmail.com>
2021-01-28 16:37:40 +0000

    hateball

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include("navigation.php","hate_ball_frontend.html");
+<?php
+include("navigation.php");
+include("hate_ball_frontend.html");
+
     echo '<p>Hello World from Roman</p>';
     echo '<h1>JUST A PLACE HOLDER</h1>'
 ?>
